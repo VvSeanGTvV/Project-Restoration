a5439ca0dec8482bb1eed4b8d5e4f89dcb9a29d2
VvSeanGTvV <gomboc.michelle@gmail.com>
2022-10-19 19:38:01 -0700

    more changes

--- src/v5mod/V5RebornMod.java
+++ src/v5mod/V5RebornMod.java
@@ -19,7 +19,7 @@ public class V5RebornMod extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
+                BaseDialog dialog = new BaseDialog("Welcome to V5 Java Edition!");
                 //dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("v5-java-mod-logoMod")).pad(20f).row();
