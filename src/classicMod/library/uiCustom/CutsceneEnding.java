1ab45eb629c9f68b22deeeff5603f98ce8a4212b
VvSeanGTvV <gomboc.michelle@gmail.com>
2023-03-09 15:02:42 -0800

    Update CutsceneEnding.java

--- src/classicMod/library/uiCustom/CutsceneEnding.java
+++ src/classicMod/library/uiCustom/CutsceneEnding.java
@@ -4,13 +4,13 @@ import arc.*;
 import arc.math.*;
 import arc.scene.actions.*;
 import arc.scene.style.*;
-import mindustry.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 import mindustry.ui.dialogs.*;
 
-import static arc.Core.atlas;
+import static arc.Core.*;
 import static arc.scene.actions.Actions.*;
+import static mindustry.Vars.*;
 
 public class CutsceneEnding extends BaseDialog {
     public CutsceneEnding() {
@@ -22,7 +22,7 @@ public class CutsceneEnding extends BaseDialog {
         buttons.defaults().size(210f, 64f);
         buttons.button("@menu", Icon.left, () -> {
             hide();
-            Vars.ui.paused.runExitSave();
+            ui.paused.runExitSave();
         });
 
         buttons.button("@continue", Icon.ok, this::hide);
@@ -33,18 +33,28 @@ public class CutsceneEnding extends BaseDialog {
         cont.clear();
 
         setTranslation(0f, -Core.graphics.getHeight());
-        color.a = 0f;
+        color.a = 255f;
 
         show(Core.scene, Actions.sequence(parallel(fadeIn(1.1f, Interp.fade), translateBy(0f, Core.graphics.getHeight(), 6f, Interp.pow5Out))));
 
         int framesTotal = 200; //TODO idk automate the video to screenshot
-        for(int i = 0; i < framesTotal ; i++){ //TODO 60 FPS or 30 FPS video ;)
-            Drawable frameI = atlas.drawable("restored-mind-frameEnd"+i);
-            frameI.draw(Core.graphics.getWidth()/2,Core.graphics.getHeight()/2,Core.graphics.getWidth(),Core.graphics.getHeight());
+        int DelayperFrame = 10;
+        for(int i = 0; i < framesTotal ; ) { //TODO 60 FPS or 30 FPS video ;)
+            int c = 0;
+            if(c<DelayperFrame) {
+                for (c = 0; c < DelayperFrame; c++) {
+                }
+                i++;
+                c = 0;
+            }
+            Drawable frameI = atlas.drawable("restored-mind-frameEnd" + i);
+            frameI.draw(Core.graphics.getWidth() / 2, Core.graphics.getHeight() / 2, Core.graphics.getWidth(), Core.graphics.getHeight());
             /*
             BaseDialog dialog = new BaseDialog("");
             dialog.cont.image(atlas.find("restored-mind-frameEnd"+i)).pad(Core.graphics.getHeight()).row();
              */
         }
+        this.hide();
+        ui.campaignComplete.show(planet);
     }
 }
