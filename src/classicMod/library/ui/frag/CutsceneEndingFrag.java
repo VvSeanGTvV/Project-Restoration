335e52c8d4fe2a9c6dc9c86730f6bf0076995517
VvSeanGTvV <gomboc.michelle@gmail.com>
2023-03-14 07:01:01 -0700

    Update CutsceneEndingFrag.java

--- src/classicMod/library/ui/frag/CutsceneEndingFrag.java
+++ src/classicMod/library/ui/frag/CutsceneEndingFrag.java
@@ -1,14 +1,25 @@
 package classicMod.library.ui.frag;
 
 import arc.scene.*;
+import arc.scene.ui.layout.*;
 import mindustry.graphics.*;
 import mindustry.ui.fragments.*;
+
+import static mindustry.Vars.*;
 
 public class CutsceneEndingFrag extends MenuFragment {
     private MenuRenderer renderer;
     @Override
     public void build(Group parent) {
         renderer = new MenuRenderer();
+        Group group = new WidgetGroup();
+        group.setFillParent(true);
+        group.visible(() -> !ui.editor.isShown());
+
+        parent.addChild(group);
+
+        parent = group;
+
         parent.setZIndex(999);
         parent.fill((x, y, w, h) -> renderer.render());
     }
