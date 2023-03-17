00eb916d0a7e86f9610b8b663f8bb80520b45fff
VvSeanGTvV <gomboc.michelle@gmail.com>
2023-03-16 20:19:27 -0700

    image bugfixes

--- src/classicMod/library/converter/ImageConverter.java
+++ src/classicMod/library/converter/ImageConverter.java
@@ -2,6 +2,7 @@ package classicMod.library.converter;
 
 
 import javax.imageio.*;
+import javax.media.jai.*;
 import java.awt.image.*;
 import java.io.*;
 
@@ -10,16 +11,19 @@ public class ImageConverter {
     public static String FileOUT = "";
     public static void main(String[] args) throws IOException {
         File inputFile = new File(FileIN); //"path/to/input/file.jpg"
-        File outputFile = new File(FileOUT); //"path/to/output/file.png"
+        //File outputFile = new File(FileOUT); //"path/to/output/file.png"
 
-        BufferedImage image = ImageIO.read(inputFile);
+        RenderedImage image = JAI.create("fileload", inputFile.getAbsolutePath());
+        RenderedImage pngImage = new RenderedImageAdapter(image);
+        //BufferedImage image = ImageIO.read(inputFile);
 
-        if (image == null) {
+        if (pngImage == null) {
             System.err.println("Error: Could not read image file.");
             return;
         }
 
-        ImageIO.write(image, "png", outputFile);
+        //ImageIO.write(image, "png", outputFile);
+        ImageIO.write(pngImage, "png", new File(FileOUT));
 
         System.out.println("Image conversion complete.");
     }
