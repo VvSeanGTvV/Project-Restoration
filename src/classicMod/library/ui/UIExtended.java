f0e9ce3dc7b60e44f6721f7648242fdf215f1685
VvSeanGTvV <gomboc.michelle@gmail.com>
2023-03-17 22:33:12 -0700

    description on it

--- src/classicMod/library/ui/UIExtended.java
+++ src/classicMod/library/ui/UIExtended.java
@@ -13,10 +13,11 @@ public class UIExtended {
 
 
     }
+    /** Get the Device's Width resolution **/
     public static int getWidth(){
         return Core.graphics.getWidth();
     }
-
+    /** Get the Device's Height resolution **/
     public static int getHeight(){
         return Core.graphics.getHeight();
     }
