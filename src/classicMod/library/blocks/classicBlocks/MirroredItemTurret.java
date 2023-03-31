e3115de47f335ecfc4a724804a29204b58a61854
VvSeanGTvV <gomboc.michelle@gmail.com>
2023-03-31 12:11:31 -0700

    Update MirroredItemTurret.java

--- src/classicMod/library/blocks/classicBlocks/MirroredItemTurret.java
+++ src/classicMod/library/blocks/classicBlocks/MirroredItemTurret.java
@@ -43,6 +43,44 @@ public class MirroredItemTurret extends ItemTurret {
             }
         }*/
 
+        protected void bullet(BulletType type, float xOffset, float yOffset, float angleOffset, Mover mover){
+            queuedBullets --;
+
+            if(dead || (!consumeAmmoOnce && !hasAmmo())) return;
+
+            float
+                    xSpread = Mathf.range(xRand),
+                    bulletX = x + Angles.trnsx(rotation - 90, shootX + xOffset + xSpread, shootY + yOffset),
+                    bulletY = y + Angles.trnsy(rotation - 90, shootX + xOffset + xSpread, shootY + yOffset),
+                    shootAngle = rotation + angleOffset + Mathf.range(inaccuracy + type.inaccuracy);
+
+            float lifeScl = type.scaleLife ? Mathf.clamp(Mathf.dst(bulletX, bulletY, targetPos.x, targetPos.y) / type.range, minRange / type.range, range() / type.range) : 1f;
+
+            //TODO aimX / aimY for multi shot turrets?
+            handleBullet(type.create(this, team, bulletX, bulletY, shootAngle, -1f, (1f - velocityRnd) + Mathf.random(velocityRnd), lifeScl, null, mover, targetPos.x, targetPos.y), xOffset, yOffset, shootAngle - rotation);
+
+            //(shootEffect == null ? type.shootEffect : shootEffect).at(bulletX, bulletY, rotation + angleOffset, type.hitColor);
+            //(smokeEffect == null ? type.smokeEffect : smokeEffect).at(bulletX, bulletY, rotation + angleOffset, type.hitColor);
+            shootSound.at(bulletX, bulletY, Mathf.random(soundPitchMin, soundPitchMax));
+
+            ammoUseEffect.at(
+                    x - Angles.trnsx(rotation, ammoEjectBack),
+                    y - Angles.trnsy(rotation, ammoEjectBack),
+                    rotation * Mathf.sign(xOffset)
+            );
+
+            if(shake > 0){
+                Effect.shake(shake, shake, this);
+            }
+
+            curRecoil = 1f;
+            heat = 1f;
+
+            if(!consumeAmmoOnce){
+                useAmmo();
+            }
+        }
+
         @Override
         public void updateShooting(){
             if(reloadCounter >= reload && !charging() && shootWarmup >= minWarmup) {
@@ -63,7 +101,9 @@ public class MirroredItemTurret extends ItemTurret {
                         totalShots ++;
                     });
                     //bullet(tile.bullet, entity.rotation);
-                    shootEffect.at(tile.drawx() + len, tile.drawy() + space, entity.rotation);
+                    shootEffect.at(tile.drawx() + len, tile.drawy() + space, rotation);
+                    shootEffect.at(tile.drawx() - len, tile.drawy() + space, rotation);
+
 
                     //Effects.effect(shootEffect, tile.drawx() + tr.x, tile.drawy() + tr.y, entity.rotation);
                 }
