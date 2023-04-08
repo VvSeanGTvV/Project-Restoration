fbc918af4c63c102e7866c69f5a8117947db7ca9
VvSeanGTvV <gomboc.michelle@gmail.com>
2023-04-08 10:14:56 -0700

    Update ExtendedStat.java

--- src/classicMod/content/ExtendedStat.java
+++ src/classicMod/content/ExtendedStat.java
@@ -1,14 +1,10 @@
 package classicMod.content;
 
-import arc.struct.*;
 import mindustry.world.meta.*;
 
-public class ExtendedStat extends Stat {
-    public static final Seq<Stat> all = new Seq<>();
-
+public class ExtendedStat {
     public static final Stat
             fuel = new Stat("fuel", StatCat.crafting)
-
     ;
 
 }
