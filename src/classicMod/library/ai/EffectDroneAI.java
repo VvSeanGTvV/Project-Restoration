04bb8f48bb683b55180e68252d1f550a526a84a3
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-06-07 21:26:48 -0700

    Update EffectDroneAI.java

--- src/classicMod/library/ai/EffectDroneAI.java
+++ src/classicMod/library/ai/EffectDroneAI.java
@@ -17,18 +17,20 @@ public class EffectDroneAI extends AIController {
         if(!(unit instanceof BuildingTetherc tether) || tether.building() == null) return;
         if(!(tether.building().block instanceof DroneCenterNew block)) return;
         if(!(tether.building() instanceof DroneCenterNewBuild build)) return;
-        if(nullify) return;
+        if(nullify) unit = null;
+        
+        if(unit != null) {
+            target = build.target;
+            if (target != null) {
+                unit.lookAt(target);
+                moveTo(target, build.target.hitSize / 5f + block.droneRange - 10f);
 
-        target = build.target;
-        if(target != null) {
-            unit.lookAt(target);
-            moveTo(target, build.target.hitSize / 5f + block.droneRange - 10f);
-
-            if (unit.within(target, block.droneRange + build.target.hitSize) && unit.within(build, block.droneRange)) {
-                build.target.apply(block.status, block.statusDuration);
+                if (unit.within(target, block.droneRange + build.target.hitSize) && unit.within(build, block.droneRange)) {
+                    build.target.apply(block.status, block.statusDuration);
+                }
+            } else {
+                moveTo(build, 5f); //Return back to Base
             }
-        } else {
-            moveTo(build, 5f); //Return back to Base
         }
     }
 
