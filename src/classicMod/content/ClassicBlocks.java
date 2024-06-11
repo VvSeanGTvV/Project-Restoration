13dec5550df979b0a13c17e74fd75ce6b7d9f3ce
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-06-10 21:21:59 -0700

    cyclone old

--- src/classicMod/content/ClassicBlocks.java
+++ src/classicMod/content/ClassicBlocks.java
@@ -52,7 +52,7 @@ public class ClassicBlocks {
 
     warpGate, //Distribution [v4]
     melter, denseSmelter, arcSmelter,  smolSeparator, //Production [v4]
-    fuseMKII, fuseMKI, salvoAlpha, arcAir, //Turret [v4]
+    fuseMKII, fuseMKI, salvoAlpha, arcAir, cycloneb57, //Turret [v4]
     plasmaDrill, //Drills [v4]
 
 
@@ -696,6 +696,101 @@ public class ClassicBlocks {
             consumePower(3.3f);
             coolant = consumeCoolant(0.1f);
         }};
+
+        cycloneb57 = new ItemTurret("cyclone-b57"){{
+            requirements(Category.turret, with(Items.copper, 200, Items.titanium, 125, Items.plastanium, 80));
+            ammo(
+                    Items.metaglass, new FlakBulletType(4f, 6){{
+                        ammoMultiplier = 2f;
+                        shootEffect = Fx.shootSmall;
+                        reloadMultiplier = 0.8f;
+                        width = 6f;
+                        height = 8f;
+                        hitEffect = Fx.flakExplosion;
+                        splashDamage = 45f;
+                        splashDamageRadius = 25f;
+                        fragBullet = new BasicBulletType(3f, 12, "bullet"){{
+                            width = 5f;
+                            height = 12f;
+                            shrinkY = 1f;
+                            lifetime = 20f;
+                            backColor = Pal.gray;
+                            frontColor = Color.white;
+                            despawnEffect = Fx.none;
+                        }};
+                        fragBullets = 4;
+                        explodeRange = 20f;
+                        collidesGround = true;
+                    }},
+                    Items.blastCompound, new FlakBulletType(4f, 8){{
+                        shootEffect = Fx.shootBig;
+                        ammoMultiplier = 5f;
+                        splashDamage = 45f;
+                        splashDamageRadius = 60f;
+                        collidesGround = true;
+
+                        status = StatusEffects.blasted;
+                        statusDuration = 60f;
+                    }},
+                    Items.plastanium, new FlakBulletType(4f, 8){{
+                        ammoMultiplier = 4f;
+                        splashDamageRadius = 40f;
+                        splashDamage = 37.5f;
+                        fragBullet = new BasicBulletType(2.5f, 12, "bullet"){{
+                            width = 10f;
+                            height = 12f;
+                            shrinkY = 1f;
+                            lifetime = 15f;
+                            backColor = Pal.plastaniumBack;
+                            frontColor = Pal.plastaniumFront;
+                            despawnEffect = Fx.none;
+                        }};
+                        fragBullets = 6;
+                        hitEffect = Fx.plasticExplosion;
+                        frontColor = Pal.plastaniumFront;
+                        backColor = Pal.plastaniumBack;
+                        shootEffect = Fx.shootBig;
+                        collidesGround = true;
+                        explodeRange = 20f;
+                    }},
+                    Items.surgeAlloy, new FlakBulletType(4.5f, 13){{
+                        ammoMultiplier = 5f;
+                        splashDamage = 50f * 1.5f;
+                        splashDamageRadius = 38f;
+                        lightning = 2;
+                        lightningLength = 7;
+                        shootEffect = Fx.shootBig;
+                        collidesGround = true;
+                        explodeRange = 20f;
+                    }}
+            );
+            shootY = 10f;
+
+            shoot = new ShootBarrel(){{
+                barrels = new float[]{
+                        0f, 1f, 0f,
+                        3f, 0f, 0f,
+                        -3f, 0f, 0f,
+                };
+            }};
+
+            recoils = 3;
+
+            reload = 8f;
+            range = 200f;
+            size = 3;
+            recoil = 1.5f;
+            recoilTime = 10;
+            rotateSpeed = 10f;
+            inaccuracy = 10f;
+            shootCone = 30f;
+            shootSound = Sounds.shootSnap;
+            coolant = consumeCoolant(0.3f);
+
+            scaledHealth = 145;
+            limitRange();
+        }};
+
     }
 
     public void load() {
