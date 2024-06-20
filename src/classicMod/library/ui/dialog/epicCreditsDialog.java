5212263c6fc42e3195e043ee63f6dd8fe3a44c53
VvSeanGTvV <64627966+VvSeanGTvV@users.noreply.github.com>
2024-06-19 23:57:49 -0700

    Update epicCreditsDialog.java

--- src/classicMod/library/ui/dialog/epicCreditsDialog.java
+++ src/classicMod/library/ui/dialog/epicCreditsDialog.java
@@ -77,7 +77,7 @@ public class epicCreditsDialog extends Dialog {
     @Override
     public void act(float delta) {
         super.act(delta);
-        float maxScroll = 3f;
+        float maxScroll = 4f;
         scrollbar += 1.25f  * Time.delta;
         cont.clearChildren();
         //in.setTranslation(0, b);;
