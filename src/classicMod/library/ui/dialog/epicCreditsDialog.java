f1f814cc0fc5832280f9500a04cb90e04136d50e
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-06-19 22:34:49 -0700

    Update epicCreditsDialog.java

--- src/classicMod/library/ui/dialog/epicCreditsDialog.java
+++ src/classicMod/library/ui/dialog/epicCreditsDialog.java
@@ -101,10 +101,10 @@ public class epicCreditsDialog extends BaseDialog {
     public void act(float delta) {
         super.act(delta);
 
-        scrollbar += 0.5f  * Time.delta;
+        scrollbar += 2f  * Time.delta;
         cont.clearChildren();
         //in.setTranslation(0, b);;
-        in.update(() -> setTranslation((float) 0, (UIExtended.getHeight() + table().maxHeight()) - scrollbar));
+        in.update(() -> setTranslation((float) 0, (UIExtended.getHeight()) - scrollbar));
         cont.add(in);
     }
 }
