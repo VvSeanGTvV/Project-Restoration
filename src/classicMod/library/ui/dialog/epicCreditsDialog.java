ddefb6e07a10055e88921c0a90281d849fbb1c5e
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-06-22 10:05:27 -0700

    Update epicCreditsDialog.java

--- src/classicMod/library/ui/dialog/epicCreditsDialog.java
+++ src/classicMod/library/ui/dialog/epicCreditsDialog.java
@@ -172,7 +172,7 @@ public class epicCreditsDialog extends Dialog {
     @Override
     public void draw() {
         float IE = ((float) graphics.getWidth() / 100);
-        staticTable.x = (((getModBundle.get(resMod.meta.name + "-credits.mobile" + app.isMobile()).length() * getModBundle.get(resMod.meta.name + "-credits.mobile" + app.isMobile()).length()) - IE) - 50f);
+        staticTable.x = (((getModBundle.get(resMod.meta.name + "-credits.mobile" + app.isMobile()).length() * getModBundle.get(resMod.meta.name + "-credits.mobile" + app.isMobile()).length()) - IE) - 50f * (IE/10);
         staticTable.y = staticTableHeight + 20f;
 
         Styles.black.draw(0, 0, UIExtended.getWidth(), UIExtended.getHeight());
