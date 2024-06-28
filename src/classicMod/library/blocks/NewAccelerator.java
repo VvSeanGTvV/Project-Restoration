1238755a70b0536cc993be807ae7c10e766991ed
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-06-28 10:35:48 -0700

    Update NewAccelerator.java

--- src/classicMod/library/blocks/NewAccelerator.java
+++ src/classicMod/library/blocks/NewAccelerator.java
@@ -196,7 +196,7 @@ public class NewAccelerator extends Block{
                     if(launchAnimation < 0.01f){ zoomStyle = 3f; Effect.shake(3f, 3f, this); } else {
                         //if(zoomStyle > 1.5f) zoomStyle -= 1f * Time.delta;
                     }
-                    launcpadTimer = Mathf.clamp(launcpadTimer + 1.15f * Time.delta);
+                    launcpadTimer = Mathf.clamp(launcpadTimer + 0.075f * Time.delta);
                     if(launcpadTimer >= 1f) stageLaunch += 1;
                 }
                 if(stageLaunch >= 2){
