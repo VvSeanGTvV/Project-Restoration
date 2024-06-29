efa904303753f8aea1d0fa277b2a810d34582870
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-06-28 17:04:02 -0700

    Update NewAccelerator.java

--- src/classicMod/library/blocks/NewAccelerator.java
+++ src/classicMod/library/blocks/NewAccelerator.java
@@ -201,7 +201,7 @@ public class NewAccelerator extends Block{
                     if(launchAnimation < 0.01f){ Effect.shake(3f, 3f, this); }
                     zoomStyle = Interp.pow3In.apply(Scl.scl(0.02f), Scl.scl(4f), 1f - launchpadTimer);
                     launchpadPrepTimer = Mathf.clamp(launchpadPrepTimer + 0.0025f * Time.delta);
-                    if(launchpadPrepTimer >= 0.5f)launchpadTimer = Mathf.clamp(launchpadTimer + 0.0075f * Time.delta);
+                    if(launchpadPrepTimer >= 0.25f)launchpadTimer = Mathf.clamp(launchpadTimer + 0.0075f * Time.delta);
                     if(launchpadTimer >= 1f) stageLaunch += 1;
                 }
                 if(stageLaunch >= 2){
@@ -373,18 +373,12 @@ public class NewAccelerator extends Block{
         //damn
         public void DrawCoreLaunchLikeLaunchpod(){
 
-            float thrustTimer = Interp.sineIn.apply(Mathf.clamp(launchpadPrepTimer * 2f));
-            //float cx = x, cy = y;
+            float thrustTimer = Interp.pow2In.apply(Mathf.clamp(launchpadPrepTimer * 2f));
+            float cx = cx(), cy = y;
             float rotation = launchpadTimer * (130f + Mathf.randomSeedRange(id(), 50f));
             float thrustOpen = 0.25f;
             float thrusterFrame = thrustTimer >= thrustOpen ? 1f : thrustTimer / thrustOpen;
             float scl = Scl.scl(4f) / renderer.getDisplayScale();
-
-            float s = region.width * region.scl() * scl * 3.6f * Interp.pow2Out.apply(1f - thrustTimer);
-
-            Draw.color(Pal.lightTrail);
-            //TODO spikier heat
-            Draw.rect("circle-shadow", x, y, s, s);
 
             Draw.z(Layer.weather - 1);
             Draw.scl(scl);
@@ -400,19 +394,19 @@ public class NewAccelerator extends Block{
 
                 Tmp.v1.setLength((size + 1f)*scl + strength*2f + offset);
                 Draw.color(Pal.accent);
-                Fill.circle(Tmp.v1.x + x, Tmp.v1.y + y, 6f * strength);
+                Fill.circle(Tmp.v1.x + x + cx, Tmp.v1.y + y, 6f * strength);
 
                 Tmp.v1.setLength((size + 1f)*scl + strength*0.5f + offset);
                 Draw.color(Color.white);
-                Fill.circle(Tmp.v1.x + x, Tmp.v1.y + y, 3.5f * strength);
+                Fill.circle(Tmp.v1.x + x + cx, Tmp.v1.y + y, 3.5f * strength);
             }
 
-            drawLandingThrusters(x, y, rotation, thrusterFrame);
+            drawLandingThrusters(x + cx, y, rotation, thrusterFrame);
 
-            Drawf.spinSprite(launching.fullIcon, x, y, rotation);
+            Drawf.spinSprite(launching.fullIcon, x + cx, y, rotation);
 
             Draw.alpha(Interp.pow4In.apply(thrusterFrame));
-            drawLandingThrusters(x, y, rotation, thrusterFrame);
+            drawLandingThrusters(x + cx, y, rotation, thrusterFrame);
             Draw.alpha(1f);
 
             if(launchpadPrepTimer >= 0.25f) {
@@ -426,67 +420,6 @@ public class NewAccelerator extends Block{
             Draw.color();
             Draw.scl();
             Draw.reset();
-
-            /*float fout = 1f - launcpadTimer;
-
-            if(renderer.isLaunching()) fout = 1f - fout;
-
-            float fin = 1f - fout;
-
-            float scl = Scl.scl(4f) / renderer.getDisplayScale();
-            float shake = 0f;
-            float s = region.width * region.scl() * scl * 3.6f * Interp.pow2Out.apply(fout);
-            float rotation = Interp.pow2In.apply(fout) * 135f;
-            x += Mathf.range(shake);
-            y += Mathf.range(shake);
-            float thrustOpen = 0.25f;
-            float thrusterFrame = fin >= thrustOpen ? 1f : fin / thrustOpen;
-            float thrusterSize = Mathf.sample(thrusterSizes, fin);
-
-            //when launching, thrusters stay out the entire time.
-            if(renderer.isLaunching()){
-                Interp i = Interp.pow2Out;
-                thrusterFrame = i.apply(Mathf.clamp(fout*13f));
-                thrusterSize = i.apply(Mathf.clamp(fout*9f));
-            }
-
-            Draw.color(Pal.lightTrail);
-            //TODO spikier heat
-            Draw.rect("circle-shadow", x, y, s, s);
-
-            Draw.scl(scl);
-
-            //draw thruster flame
-            float strength = (1f + (size - 3)/2.5f) * scl * thrusterSize * (0.95f + Mathf.absin(2f, 0.1f));
-            float offset = (size - 3) * 3f * scl;
-
-            for(int i = 0; i < 4; i++){
-                Tmp.v1.trns(i * 90 + rotation, 1f);
-
-                Tmp.v1.setLength((size * tilesize/2f + 1f)*scl + strength*2f + offset);
-                Draw.color(Pal.accent);
-                Fill.circle(Tmp.v1.x + x, Tmp.v1.y + y, 6f * strength);
-
-                Tmp.v1.setLength((size * tilesize/2f + 1f)*scl + strength*0.5f + offset);
-                Draw.color(Color.white);
-                Fill.circle(Tmp.v1.x + x, Tmp.v1.y + y, 3.5f * strength);
-            }
-
-            drawLandingThrusters(x, y, rotation, thrusterFrame);
-
-            Drawf.spinSprite(region, x, y, rotation);
-
-            Draw.alpha(Interp.pow4In.apply(thrusterFrame));
-            drawLandingThrusters(x, y, rotation, thrusterFrame);
-            Draw.alpha(1f);
-
-            //if(teamRegions[build.team.id] == teamRegion) Draw.color(Team.sharded.color);
-
-            Drawf.spinSprite(launching.fullIcon, x, y, rotation);
-
-            Draw.color();
-            Draw.scl();
-            Draw.reset();*/
         }
 
         public void drawThrusters(float x, float y, float rotation, float frame){
