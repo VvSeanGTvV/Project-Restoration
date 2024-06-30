3abff4383cae0bdc5bec741be65607797c744e65
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-06-29 17:55:50 -0700

    more stuf

--- src/classicMod/library/ui/dialog/ContentUnlockDebugDialog.java
+++ src/classicMod/library/ui/dialog/ContentUnlockDebugDialog.java
@@ -1,10 +1,20 @@
 package classicMod.library.ui.dialog;
 
+import arc.Core;
+import arc.Events;
 import arc.scene.ui.Dialog;
+import arc.scene.ui.layout.Scl;
 import arc.scene.ui.layout.Table;
+import arc.util.Scaling;
 import mindustry.Vars;
+import mindustry.game.EventType;
+import mindustry.type.Sector;
+import mindustry.type.SectorPreset;
 import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
+
+import static mindustry.Vars.*;
+import static mindustry.Vars.control;
 
 public class ContentUnlockDebugDialog extends BaseDialog {
 
@@ -16,12 +26,12 @@ public class ContentUnlockDebugDialog extends BaseDialog {
             {
                 for (var Content : Vars.content.items()){
                     table(Styles.grayPanel, info -> {
-                        info.image(Content.fullIcon).size(32f).pad(10f).left();
+                        info.image(Content.fullIcon).size(32f).scaling(Scaling.fit).pad(10f).left();
                         info.add(Content.localizedName).left();
 
                         info.table(yes -> {
-                            yes.button("@lock", Content::clearUnlock).size(105f, 32f).pad(2.5f);
-                            yes.button("@unlock", Content::unlock).size(105f, 32f).pad(2.5f);
+                            yes.button("@lock", Content::clearUnlock).size(105f, 64f).pad(2.5f);
+                            yes.button("@unlock", Content::unlock).size(105f, 64f).pad(2.5f);
                         }).right().pad(2.5f);
                     }).left();
                     row();
@@ -33,12 +43,47 @@ public class ContentUnlockDebugDialog extends BaseDialog {
             {
                 for (var Content : Vars.content.liquids()){
                     table(Styles.grayPanel, info -> {
-                        info.image(Content.fullIcon).size(32f).pad(10f).left();
+                        info.image(Content.fullIcon).size(32f).scaling(Scaling.fit).pad(10f).left();
                         info.add(Content.localizedName).left();
 
                         info.table(yes -> {
-                            yes.button("@lock", Content::clearUnlock).size(105f, 32f).pad(2.5f);
-                            yes.button("@unlock", Content::unlock).size(105f, 32f).pad(2.5f);
+                            yes.button("@lock", Content::clearUnlock).size(105f, 64f).pad(2.5f);
+                            yes.button("@unlock", Content::unlock).size(105f, 64f).pad(2.5f);
+                        }).right().pad(2.5f);
+                    }).left();
+                    row();
+                }
+            }
+        });
+
+        cont.pane(new Table() {
+            {
+                for (var Content : Vars.content.units()){
+                    table(Styles.grayPanel, info -> {
+                        info.image(Content.fullIcon).size(32f).scaling(Scaling.fit).pad(10f).left();
+                        info.add(Content.localizedName).left();
+
+                        info.table(yes -> {
+                            yes.button("@lock", Content::clearUnlock).size(105f, 64f).pad(2.5f);
+                            yes.button("@unlock", Content::unlock).size(105f, 64f).pad(2.5f);
+                        }).right().pad(2.5f);
+                    }).left();
+                    row();
+                }
+            }
+        });
+
+        cont.pane(new Table() {
+            {
+                for (var Content : Vars.content.sectors()){
+                    table(Styles.grayPanel, info -> {
+                        info.image(Content.fullIcon).size(32f).scaling(Scaling.fit).pad(10f).left();
+                        info.add(Content.localizedName).left();
+
+                        info.table(yes -> {
+                            yes.button("@lock", Content::clearUnlock).size(105f, 64f).pad(2.5f);
+                            yes.button("@unlock", Content::unlock).size(105f, 64f).pad(2.5f);
+                            yes.button("@launch-to", () -> StartSector(Content)).size(105f, 64f).pad(2.5f);
                         }).right().pad(2.5f);
                     }).left();
                     row();
@@ -48,4 +93,11 @@ public class ContentUnlockDebugDialog extends BaseDialog {
 
         show();
     }
+    public void StartSector(SectorPreset to){
+        var sector = to.sector;
+
+        Events.fire(new EventType.SectorLaunchEvent(sector));
+        control.playSector(sector);
+    }
+
 }
