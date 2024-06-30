dd1ea951a392b874f29eb05f31021a9ac6e27bd5
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-06-29 17:49:47 -0700

    debug panel

--- src/classicMod/library/ui/dialog/ContentUnlockDebugDialog.java
+++ src/classicMod/library/ui/dialog/ContentUnlockDebugDialog.java
@@ -20,10 +20,27 @@ public class ContentUnlockDebugDialog extends BaseDialog {
                         info.add(Content.localizedName).left();
 
                         info.table(yes -> {
-                            yes.button("@locked", Content::clearUnlock).size(105f, 32f).pad(2.5f);
+                            yes.button("@lock", Content::clearUnlock).size(105f, 32f).pad(2.5f);
                             yes.button("@unlock", Content::unlock).size(105f, 32f).pad(2.5f);
                         }).right().pad(2.5f);
-                    });
+                    }).left();
+                    row();
+                }
+            }
+        });
+
+        cont.pane(new Table() {
+            {
+                for (var Content : Vars.content.liquids()){
+                    table(Styles.grayPanel, info -> {
+                        info.image(Content.fullIcon).size(32f).pad(10f).left();
+                        info.add(Content.localizedName).left();
+
+                        info.table(yes -> {
+                            yes.button("@lock", Content::clearUnlock).size(105f, 32f).pad(2.5f);
+                            yes.button("@unlock", Content::unlock).size(105f, 32f).pad(2.5f);
+                        }).right().pad(2.5f);
+                    }).left();
                     row();
                 }
             }
