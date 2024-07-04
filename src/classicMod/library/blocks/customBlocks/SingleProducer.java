488f35ba7699001a10a82b49cec5293278346a2f
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-07-04 09:37:29 -0700

    Update SingleProducer.java

--- src/classicMod/library/blocks/customBlocks/SingleProducer.java
+++ src/classicMod/library/blocks/customBlocks/SingleProducer.java
@@ -1,12 +1,16 @@
 package classicMod.library.blocks.customBlocks;
 
 import arc.math.Mathf;
+import arc.struct.ObjectMap;
 import mindustry.gen.Building;
+import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.payloads.BlockProducer;
 import mindustry.world.blocks.payloads.BuildPayload;
 import mindustry.world.blocks.payloads.Payload;
+import mindustry.world.consumers.ConsumeItemDynamic;
 
+import static mindustry.Vars.state;
 import static mindustry.Vars.tilesize;
 import static mindustry.type.ItemStack.with;
 
@@ -14,7 +18,21 @@ public class SingleProducer extends BlockProducer {
     public Block produce = null;
     public SingleProducer(String name) {
         super(name);
-        consumeItems(produce.requirements);
+
+        ObjectMap<Block, ItemStack[]> stacks = new ObjectMap<>();
+        consume(new ConsumeItemDynamic((BlockProducerBuild e) -> {
+            Block block = e.recipe();
+
+            if(block != null){
+                ItemStack[] clone = stacks.get(block, () -> ItemStack.copy(block.requirements));
+                for(int i = 0; i < clone.length; i++){
+                    clone[i].amount = Mathf.ceil(block.requirements[i].amount * state.rules.buildCostMultiplier);
+                }
+                return clone;
+            }else{
+                return ItemStack.empty;
+            }
+        }));
     }
 
     public class SingleProducerBuild extends BlockProducerBuild {
