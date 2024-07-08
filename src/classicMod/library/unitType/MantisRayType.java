99ba8cbf04f993063f96c2e06e7ffeb126c37c8c
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-07-08 14:06:15 -0700

    Update MantisRayType.java

--- src/classicMod/library/unitType/MantisRayType.java
+++ src/classicMod/library/unitType/MantisRayType.java
@@ -96,18 +96,18 @@ public class MantisRayType extends UnitType {
 
         Draw.rect(region, unit.x, unit.y, unit.rotation - 90);
 
-        Log.info("lrot " + lastRot + " | lrote " + lastRotEnd + " | rot " + unit.rotation);
+        Log.info("lrot " + lastRot + " | lrote " + lastRotEnd + " | rot " + Mathf.ceil(unit.rotation));
         Log.info("lrotb " + (unit.rotation == 0 && lastRot > 0) + " | lroteb " + (unit.rotation == 0 && lastRotEnd > 0));
         lastRot = Mathf.lerpDelta(lastRot, unit.rotation, 0.25f);
         float lRot0 = lastRot - unit.rotation;
-        if (unit.rotation == 0 && lastRot > 0) lRot0 = lastRotEnd + unit.rotation;
+        if (Mathf.ceil(unit.rotation) == 0 && lastRot > 0) lRot0 = lastRotEnd + unit.rotation;
         float yBody = (TailBody.height / 7.5f) + 0f;
         Tmp.v1.trns(unit.rotation + lRot0 - 90, 0, yBody);
         Draw.rect(TailBody, unit.x - Tmp.v1.x, unit.y - Tmp.v1.y, unit.rotation + lRot0 - 90);
 
         lastRotEnd = Mathf.lerpDelta(lastRotEnd, unit.rotation, 0.1f);
         float lRot1 = lastRotEnd - unit.rotation;
-        if (unit.rotation == 0 && lastRotEnd > 0) lRot1 = lastRotEnd + unit.rotation;
+        if (Mathf.ceil(unit.rotation) == 0 && lastRotEnd > 0) lRot1 = lastRotEnd + unit.rotation;
         yBody += (TailBodyEnd.height / 6.15f) + 0f;
         Tmp.v1.trns(unit.rotation + lRot1 - 90, 0, yBody);
         Draw.rect(TailBodyEnd, unit.x - Tmp.v1.x, unit.y - Tmp.v1.y, unit.rotation + lRot1 - 90);
