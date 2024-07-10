9f4cca39f2767c6140151f84f82be7756c2b0733
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-07-10 16:38:05 -0700

    Update MantisRayAI.java

--- src/classicMod/library/ai/MantisRayAI.java
+++ src/classicMod/library/ai/MantisRayAI.java
@@ -6,10 +6,9 @@ import classicMod.library.unitType.MantisRayType;
 import mindustry.entities.units.AIController;
 
 public class MantisRayAI extends AIController {
-
     @Override
-    public void updateUnit() {
-        super.updateUnit();
+    public void updateVisuals() {
+        super.updateVisuals();
         if(unit.type instanceof MantisRayType type){
             type.timer += Time.delta / 20f;
             type.lastRot = Mathf.slerpDelta(type.lastRot, unit.rotation, 0.35f);
