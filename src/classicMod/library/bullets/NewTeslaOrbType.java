e0481004e224f0149a80f863e27a61e3e121fc75
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-10-02 18:42:10 -0700

    Update NewTeslaOrbType.java

--- src/classicMod/library/bullets/NewTeslaOrbType.java
+++ src/classicMod/library/bullets/NewTeslaOrbType.java
@@ -89,7 +89,7 @@ public class NewTeslaOrbType extends BulletType {
             var y = b.y;
             var currentRange = range;
 
-            var target = Damage.linecast(b, x, y, b.rotation(), currentRange * b.fout());
+            var target = Damage.linecast(b, x, y, b.rotation(), lightningLength + Mathf.random(lightningLengthRand));
 
             if(tlist.size > 0){
                 var current = tlist.get(tlist.size - 1);
