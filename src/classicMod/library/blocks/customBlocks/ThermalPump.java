59f5ac9df10651fce72fb82ffa474103514096c3
VvSeanGTvV <gomboc.michelle@gmail.com>
2024-10-05 17:27:02 -0700

    Update ThermalPump.java

--- src/classicMod/library/blocks/customBlocks/ThermalPump.java
+++ src/classicMod/library/blocks/customBlocks/ThermalPump.java
@@ -2,19 +2,43 @@ package classicMod.library.blocks.customBlocks;
 
 import arc.Core;
 import arc.graphics.Color;
-import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.*;
 import arc.math.Mathf;
-import arc.util.Time;
+import arc.util.*;
+import mindustry.game.Team;
+import mindustry.logic.LAccess;
 import mindustry.type.Liquid;
 import mindustry.world.*;
+import mindustry.world.blocks.liquid.LiquidBlock;
 import mindustry.world.blocks.production.Pump;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
 
 import static mindustry.Vars.*;
 
-public class ThermalPump extends Pump {
+public class ThermalPump extends LiquidBlock {
+    /** Pump amount per tile. */
+    public float pumpAmount = 0.2f;
+    /** Interval in-between item consumptions, if applicable. */
+    public float consumeTime = 60f * 5f;
+    public float warmupSpeed = 0.019f;
+    public DrawBlock drawer = new DrawMulti(new DrawDefault(), new DrawPumpLiquid());
 
     public ThermalPump(String name) {
         super(name);
+        group = BlockGroup.liquids;
+        floating = true;
+        envEnabled = Env.terrestrial;
+    }
+
+    @Override
+    public void setStats(){
+        super.setStats();
+        stats.add(Stat.output, 60f * pumpAmount * size * size, StatUnit.liquidSecond);
+    }
+
+    protected boolean canPump(Tile tile){
+        return tile != null && tile.floor().liquidDrop != null;
     }
 
     @Override
@@ -48,9 +72,93 @@ public class ThermalPump extends Pump {
             Draw.reset();
             Draw.rect(liquidDrop.fullIcon, dx, dy, s * ratio, s);
         }
+
     }
 
-    public class ThermalPumpBuild extends PumpBuild {
+
+    @Override
+    public void load(){
+        super.load();
+        drawer.load(this);
+    }
+
+    @Override
+    public TextureRegion[] icons(){
+        return drawer.finalIcons(this);
+    }
+
+    @Override
+    public boolean canPlaceOn(Tile tile, Team team, int rotation){
+        if(isMultiblock()){
+            Liquid last = null;
+            for(Tile other : tile.getLinkedTilesAs(this, tempTiles)){
+                if(other.floor().liquidDrop == null) continue;
+                if(other.floor().liquidDrop != last && last != null) return false;
+                last = other.floor().liquidDrop;
+            }
+            return last != null;
+        }else{
+            return canPump(tile);
+        }
+    }
+
+    @Override
+    public void setBars(){
+        super.setBars();
+
+        //replace dynamic output bar with own custom bar
+        addLiquidBar((Pump.PumpBuild build) -> build.liquidDrop);
+    }
+
+    public class ThermalPumpBuild extends LiquidBuild {
+        public float warmup, totalProgress;
+        public float consTimer;
+        public float amount = 0f;
+        public @Nullable Liquid liquidDrop = null;
+
+        @Override
+        public void draw(){
+            drawer.draw(this);
+        }
+
+        @Override
+        public void drawLight(){
+            super.drawLight();
+            drawer.drawLight(this);
+        }
+
+        @Override
+        public void pickedUp(){
+            amount = 0f;
+        }
+
+        @Override
+        public double sense(LAccess sensor){
+            if(sensor == LAccess.efficiency) return shouldConsume() ? efficiency : 0f;
+            if(sensor == LAccess.totalLiquids) return liquidDrop == null ? 0f : liquids.get(liquidDrop);
+            return super.sense(sensor);
+        }
+
+        @Override
+        public void onProximityUpdate(){
+            super.onProximityUpdate();
+
+            amount = 0f;
+            liquidDrop = null;
+
+            for(Tile other : tile.getLinkedTiles(tempTiles)){
+                if(canPump(other)){
+                    liquidDrop = other.floor().liquidDrop;
+                    amount += other.floor().liquidMultiplier;
+                }
+            }
+        }
+
+        @Override
+        public boolean shouldConsume(){
+            return liquidDrop != null && liquids.get(liquidDrop) < liquidCapacity - 0.01f && enabled;
+        }
+
         @Override
         public void updateTile(){
             if(efficiency > 0 && liquidDrop != null){
@@ -74,6 +182,22 @@ public class ThermalPump extends Pump {
             if(liquidDrop != null){
                 dumpLiquid(liquidDrop);
             }
+        }
+
+
+        @Override
+        public float warmup(){
+            return warmup;
+        }
+
+        @Override
+        public float progress(){
+            return Mathf.clamp(consTimer / consumeTime);
+        }
+
+        @Override
+        public float totalProgress(){
+            return totalProgress;
         }
     }
 }
