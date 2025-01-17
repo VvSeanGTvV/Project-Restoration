67342ff33f2eeb472bac0252040994c0cb188b63
VvSeanGTvV <gomboc.michelle@gmail.com>
2025-01-17 07:59:45 -0800

    Update NeoplasmBlock.java

--- src/classicMod/library/blocks/neoplasiaBlocks/NeoplasmBlock.java
+++ src/classicMod/library/blocks/neoplasiaBlocks/NeoplasmBlock.java
@@ -208,7 +208,8 @@ public class NeoplasmBlock extends Block {
             for (int dx = (tile.x - half); dx < (tile.x + half); dx++){
                 for (int dy = (tile.y - half); dy < (tile.y + half); dy++) {
                     Tile tile = Vars.world.tile(dx, dy);
-                    if (tile != null && (tile.block() == Blocks.air || tile.block() == ClassicBlocks.cord)) {
+                    if (tile != null) {
+                        if (tile.block() != Blocks.air || tile.block() != ClassicBlocks.cord) continue;
                         spaceAvaliable++;
                     }
                 }
