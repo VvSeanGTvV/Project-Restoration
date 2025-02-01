93bae6c3140b41e9ff7a46f2922113c8e62995f6
VvSeanGTvV <gomboc.michelle@gmail.com>
2025-01-31 22:00:08 -0800

    test

--- src/classicMod/library/ui/dialog/NewLaunchDialog.java
+++ src/classicMod/library/ui/dialog/NewLaunchDialog.java
@@ -1,6 +1,7 @@
 package classicMod.library.ui.dialog;
 
 import arc.Core;
+import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
 import arc.input.KeyCode;
 import arc.math.Mathf;
@@ -20,11 +21,13 @@ import mindustry.type.*;
 import mindustry.ui.*;
 import mindustry.ui.dialogs.*;
 
-public class NewLaunchDialog extends Dialog {
+public class NewLaunchDialog extends Dialog implements PlanetRenderer.PlanetInterfaceRenderer {
     public final PlanetRenderer planets;
     public PlanetParams state = new PlanetParams();
     public float zoom;
-    public Label hoverLabel;
+    public Label hoverLabel, selectLabel;
+    protected Planet hoverPlanet, selectPlanet, fromPlanet;
+    protected Sector test;
     protected boolean wasPaused;
     protected boolean shouldPause;
 
@@ -37,6 +40,7 @@ public class NewLaunchDialog extends Dialog {
                 this.titleFontColor = Pal.accent;
             }
         });
+
         this.hidden(() -> {
             if (this.shouldPause && Vars.state.isGame() && !Vars.net.active() && !this.wasPaused) {
                 Vars.state.set(GameState.State.playing);
@@ -58,6 +62,11 @@ public class NewLaunchDialog extends Dialog {
 
         this.setFillParent(true);
         this.addListener(new InputListener() {
+            @Override
+            public boolean mouseMoved(InputEvent event, float x, float y) {
+                return super.mouseMoved(event, x, y);
+            }
+
             public boolean keyDown(InputEvent event, KeyCode key) {
                 if (event.targetActor != NewLaunchDialog.this || key != KeyCode.escape && key != KeyCode.back && key != Core.keybinds.get(Binding.planet_map).key) {
                     return false;
@@ -78,6 +87,7 @@ public class NewLaunchDialog extends Dialog {
                 }
             }
         });
+
         this.dragged((cx, cy) -> {
             if (Core.input.getTouches() <= 1) {
 
@@ -118,53 +128,108 @@ public class NewLaunchDialog extends Dialog {
             }
         });
 
+        this.selectLabel = new Label("");
         this.hoverLabel = new Label("");
 
         this.hoverLabel.setStyle(Styles.outlineLabel);
         this.hoverLabel.setAlignment(1);
 
+        this.selectLabel.setStyle(Styles.outlineLabel);
+        this.selectLabel.setAlignment(1);
+
+        this.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if (hoverPlanet != null && hoverPlanet != selectPlanet){
+                    selectPlanet = hoverPlanet;
+                    updateSelect();
+                }
+            }
+        });
+
         addBack();
     }
 
+    void updateSelect(){
+        state.planet = selectPlanet;
+        state.otherCamPos = selectPlanet.position;
+        state.otherCamAlpha = 0f;
+    }
+
     public void showPlanetLaunch(Sector sector) {
-        state.planet = sector.planet;
+        state.zoom = this.zoom = sector.planet.minZoom;
+        state.planet = fromPlanet = hoverPlanet = selectPlanet = sector.planet;
         state.otherCamPos = sector.planet.position;
+        test = sector;
         state.otherCamAlpha = 0f;
         super.show();
     }
 
+    public void addCloseListener() {
+        this.closeOnBack();
+    }
     void addBack() {
         this.buttons.button("@back", Icon.left, this::hide).size(200.0F, 54.0F).pad(2.0F).bottom();
+        this.addCloseListener();
     }
 
     @Override
     public void draw() {
         planets.render(state);
-        Draw.flush();
         super.draw();
     }
 
+    float d = 0;
     @Override
     public void act(float delta) {
-        Planet planetHover = null;
+        hoverPlanet = null;
         for (var planet : Vars.content.planets()) {
             var pos = planet.intersect(this.planets.cam.getMouseRay(), 1.17F * planet.radius);
             if (!planet.accessible || !planet.visible) continue;
             if (pos != null) {
-                planetHover = planet;
+                hoverPlanet = planet;
                 break;
             }
         }
-        if (planetHover != null && !Vars.mobile) {
+
+        if (selectPlanet != null && fromPlanet != selectPlanet) {
+            this.addChild(this.selectLabel);
+            this.selectLabel.toFront();
+            this.selectLabel.touchable = Touchable.disabled;
+            this.selectLabel.color.a = this.state.uiAlpha;
+            Vec3 pos = this.planets.cam.project(Tmp.v31.set(selectPlanet.position).setLength(0F * selectPlanet.radius).rotate(Vec3.Y, -selectPlanet.getRotation()).add(selectPlanet.position));
+            this.selectLabel.setPosition(pos.x - Core.scene.marginLeft, pos.y - Core.scene.marginBottom, 1);
+            this.selectLabel.getText().setLength(0);
+            StringBuilder tx = this.selectLabel.getText();
+            if (d < 7){
+                tx.append("[green]>  [lime]").append(selectPlanet.localizedName).append("[green]  <");
+            } if (d < 15 && d > 7){
+                tx.append("[green]> [lime]").append(selectPlanet.localizedName).append("[green] <");
+            } if (d < 23 && d > 15){
+                tx.append("[green]>[lime]").append(selectPlanet.localizedName).append("[green]<");
+            } if (d > 23){
+                d = 0;
+            }
+            d += Time.delta;
+
+
+            this.selectLabel.invalidateHierarchy();
+        } else {
+            this.selectLabel.remove();
+        }
+
+        if (hoverPlanet != null && !Vars.mobile && selectPlanet != hoverPlanet) {
             this.addChild(this.hoverLabel);
             this.hoverLabel.toFront();
             this.hoverLabel.touchable = Touchable.disabled;
             this.hoverLabel.color.a = this.state.uiAlpha;
-            Vec3 pos = this.planets.cam.project(Tmp.v31.set(planetHover.position).setLength(0F * planetHover.radius).rotate(Vec3.Y, -planetHover.getRotation()).add(planetHover.position));
+            Vec3 pos = this.planets.cam.project(Tmp.v31.set(hoverPlanet.position).setLength(0F * hoverPlanet.radius).rotate(Vec3.Y, -hoverPlanet.getRotation()).add(hoverPlanet.position));
             this.hoverLabel.setPosition(pos.x - Core.scene.marginLeft, pos.y - Core.scene.marginBottom, 1);
             this.hoverLabel.getText().setLength(0);
             StringBuilder tx = this.hoverLabel.getText();
-            tx.append("[accent][[ [white]").append(planetHover.localizedName).append("[accent] ]");
+            tx.append("[accent][[ [white]").append(hoverPlanet.localizedName).append("[accent] ]");
+            
+
 
             this.hoverLabel.invalidateHierarchy();
         } else {
@@ -173,5 +238,11 @@ public class NewLaunchDialog extends Dialog {
 
         this.state.zoom = Mathf.lerpDelta(this.state.zoom, this.zoom, 0.4F);
         this.state.uiAlpha = Mathf.lerpDelta(this.state.uiAlpha, (float)Mathf.num(this.state.zoom < 1.9F), 0.1F);
+        super.act(delta);
+    }
+
+
+    public void renderProjections(Planet planet) {
+
     }
 }
